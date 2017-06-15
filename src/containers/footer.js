e75b8b04940f9e5e29c757f5efbfc38fc885eed4
cwebsterz <cwebsterzornes@live.com>
2017-06-15 16:38:26 -0400

    corect link in footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -6,7 +6,10 @@ export default props => {
 		<div className="avenir">
 			<footer className="pv4 ph3 ph5-m ph6-l mid-gray">
 				<small className="f6 db tc">
-					<a className="ttu link dim black" href="https://github.com/cwebsterz">
+					<a
+						className="ttu link dim black"
+						href="https://github.com/cwebsterz/hello-incrementor"
+					>
 						@cwebsterz
 					</a>
 				</small>
